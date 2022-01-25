cc6d8b296deb21b2837a0387120924f7fbb86606
Hyunsoo Jeon <hsprime85@gmail.com>
2022-01-24 22:20:03 -0800

    adding menus

--- 2-C#/Poke/PokeUI/Program.cs
+++ 2-C#/Poke/PokeUI/Program.cs
@@ -1,4 +1,30 @@
-﻿using  PokeModel;
+﻿
+using PokeUI;
 
-Ability ab = new Ability();
-ab.PP = -1;
+
+
+bool repeat = true;
+IMenu menu = new MainMenu();
+
+while (repeat)
+{
+    Console.Clear();
+    menu.Display();
+    string ans = menu.UserChoice();
+    
+    switch (ans)
+    {
+        case "AddPokemon":
+            menu = new AddPokeMenu();
+            break;
+        case "MainMenu":
+            menu = new MainMenu();
+            break;
+        case "Exit":
+            repeat = false;
+            break;
+        default:
+            Console.WriteLine("Page does not exist!");
+            break;
+    }
+}
